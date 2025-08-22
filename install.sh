80c81908a1b03c0ca99ba8a9302329439c17c588
kike <enriquelagui@outlook.com>
2025-08-22 18:00:01 +0200

    Add k9s

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ stow --target "$HOME" zsh
 stow --target "$HOME" ideavim
 stow --target "$HOME" tmux
 stow --target "$HOME" ghostty
+stow --target "$HOME" k9s
 stow --target "$HOME" zellij
 stow --target "$HOME" hypr
 stow --target "$HOME" waybar
