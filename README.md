d49725022d676d93c40ee6400c67fc4a45b7f4ba
kike <enriquelagui@outlook.com>
2025-08-22 07:59:44 +0200

    Use Stow for dotfile management

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # My Dotfiles
 
+Requires `stow` for managing the symlinks.
+
 ## Installation
 
 ```bash
-git clone git@github.com:kikefdezl/dotfiles.git 
+git clone git@github.com:kikefdezl/dotfiles.git $HOME/.dotfiles
+cd $HOME/.dotfiles
+
 ./install.sh
 ```
-
-Installs:
-
-- `.zshrc`
-- `.ideavimrc`
-- `.tmux.conf`
--- install.sh
+++ install.sh
@@ -1,26 +1,7 @@
 #!/bin/zsh
 
-# .zshrc
-cp .zshrc ~/
-source ~/.zshrc
-echo "Installed ~/.zshrc"
-
-# .ideavimrc
-cp .ideavimrc ~/
-echo "Installed ~/.ideavimrc"
-
-# tmux
-cp .tmux.conf ~/
-tmux source-file ~/.tmux.conf
-echo "Installed ~/.tmux.conf"
-cp -r .tmux_sessions ~/
-echo "Installed ~/.tmux_sessions/"
-
-# ghostty config
-cp -r .config/ghostty ~/.config
-echo "Installed ~/.config/ghostty"
-
-# zellij config
-cp -r .config/zellij ~/.config
-echo "Installed ~/.config/zellij"
-
+stow --target "$HOME" zsh
+stow --target "$HOME" ideavim
+stow --target "$HOME" tmux
+stow --target "$HOME" ghostty
+stow --target "$HOME" zellij
