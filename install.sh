1cb5e28e57ddbd28196032aaaa8e08f998f86f56
kike <enriquelagui@outlook.com>
2025-09-16 14:27:12 +0200

    Add conditional install for desktop/laptop

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/zsh
 
-stow --target "$HOME" zsh
-stow --target "$HOME" ideavim
-stow --target "$HOME" tmux
-stow --target "$HOME" ghostty
-stow --target "$HOME" k9s
-stow --target "$HOME" zellij
-stow --target "$HOME" hypr
-stow --target "$HOME" waybar
+# desktop
+if [[ "$(hostname)" == "arch-desktop" ]]; then
+  stow --target "$HOME" zsh
+  stow --target "$HOME" ideavim
+  stow --target "$HOME" tmux
+  stow --target "$HOME" ghostty
+  stow --target "$HOME" k9s
+  stow --target "$HOME" zellij
+  stow --target "$HOME" hypr
+  stow --target "$HOME" waybar
+fi
+
+# laptop
+if [[ "$(hostname)" == "kike-ThinkPad" ]]; then
+  stow --target "$HOME" zsh
+  stow --target "$HOME" ghostty
+  stow --target "$HOME" k9s
+fi
