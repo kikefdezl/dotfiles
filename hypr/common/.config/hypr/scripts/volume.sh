a95f9abe879cd5a21abbe48400faa6f0780c8108
kike <enriquelagui@outlook.com>
2026-01-17 15:27:51 +0100

    Update dunst aesthetic and volume script

--- hypr/common/.config/hypr/scripts/volume.sh
+++ hypr/common/.config/hypr/scripts/volume.sh
@@ -8,29 +8,31 @@ function is_mute {
     wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED
 }
 
-function send_notification {
+function send_volume_notification {
     volume=$(get_volume)
-    # Make the bar with the special character ─
-    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
-    # Send the notification
-    dunstify -i audio-volume-high -t 1000 -r 2593 -u normal "Volume: $volume%" "$bar"
+    dunstify -h string:x-canonical-private-synchronous:audio "Volume - $volume%" -h int:value:$volume
 }
 
-case $1 in
-    up)
-        wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+
-        send_notification
-        ;;
-    down)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-
-        send_notification
-        ;;
-    mute)
-        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        if is_mute ; then
-            dunstify -i audio-volume-muted -t 1000 -r 2593 -u normal "Muted" ""
-        else
-            send_notification
-        fi
-        ;;
-esac
+function send_mute_notification {
+    dunstify -h string:x-canonical-private-synchronous:audio "Muted" ""
+}
+
+if is_mute; then 
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    send_volume_notification
+else
+    case $1 in
+        up)
+            wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+
+            send_volume_notification
+            ;;
+        down)
+            wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-
+            send_volume_notification
+            ;;
+        mute)
+            wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+            send_mute_notification
+            ;;
+    esac
+fi
