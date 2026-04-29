a33b71ebac935dd2942527ea482260243ca3a4c2
kike <enriquelagui@outlook.com>
2026-04-29 09:12:46 +0200

    remove keystats

--- waybar/.config/waybar/modules/keystats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-keystats_file="/var/lib/keystats/keystats.json"
-
-# the sed makes the number comma separated, so 1234567 becomes 1,234,567
-text=$(jq '.TOTAL' $keystats_file | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-
-echo "{\"text\": \"$text\",\"class\":\"keystats\"}"
