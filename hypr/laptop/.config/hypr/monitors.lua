8d4032b079afe77c7a85c49fe4cc13e548ff55de
Kike <kike@laguilho.at>
2026-08-02 21:37:41 +0200

    fix(hypr): monitor configuration for laptop

--- hypr/laptop/.config/hypr/monitors.lua
+++ hypr/laptop/.config/hypr/monitors.lua
@@ -2,7 +2,7 @@ local built_in = "eDP-1"
 local hdmi = "HDMI-A-1"
 local display_port = "DP-1" -- via USB
 
-hl.monitor({ output = built_in, mode = "1920x1080", position = 0x0, scale = 1 })
+hl.monitor({ output = built_in, mode = "1920x1080", position = "0x0", scale = 1 })
 hl.monitor({ output = hdmi, mode = "3840x2160", position = "-2560x0", scale = 1.5 })
 hl.monitor({ output = display_port, mode = "3840x2160", position = "-2560x0", scale = 1.5 })
 
