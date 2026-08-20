482c6ac3e4883ee4c6577d24064b3d9e281d71ef
Kike <kike@laguilho.at>
2026-08-20 10:01:02 +0200

    update laptop configuration

--- hypr/laptop/.config/hypr/monitors.lua
+++ hypr/laptop/.config/hypr/monitors.lua
@@ -4,9 +4,9 @@ local display_port = "DP-1" -- via USB
 
 hl.monitor({ output = built_in, mode = "1920x1080", position = "0x0", scale = 1 })
 hl.monitor({ output = hdmi, mode = "3840x2160", position = "-2560x0", scale = 1.5 })
-hl.monitor({ output = display_port, mode = "3840x2160", position = "-2560x0", scale = 1.5 })
+hl.monitor({ output = display_port, mode = "3840x2160", position = "0x-1440", scale = 1.5 })
 
 MONITORS = {
-	MAIN = hdmi,
+	MAIN = display_port,
 	SECONDARY = built_in,
 }
