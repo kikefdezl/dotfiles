caa01a7a519733a9be2976d2a2877c31c9a80f23
Kike <kike@laguilho.at>
2026-08-20 10:38:00 +0200

    fix(waybar): vpn module for robotise networks

--- waybar/.config/waybar/modules/vpn.sh
+++ waybar/.config/waybar/modules/vpn.sh
@@ -2,7 +2,7 @@
 
 ACTIVE_CONNECTIONS=$(nmcli -t -f NAME,TYPE connection show --active)
 
-if echo "$ACTIVE_CONNECTIONS" | grep -iq "robotise.*"; then
+if echo "$ACTIVE_CONNECTIONS" | grep -iq "robotise.*vpn.*"; then
     echo '{"text":" VPN","class":"robotise"}'
 elif echo "$ACTIVE_CONNECTIONS" | grep -iq "Proton.*"; then
     echo '{"text":" VPN","class":"proton"}'
