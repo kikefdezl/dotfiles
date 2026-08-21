f3e1f2e69b390635287f1511d69a2c8eb00b31f9
Kike <kike@laguilho.at>
2026-08-21 10:56:28 +0200

    feat(hyprl): update monitor configuration

--- hypr/desktop/.config/hypr/monitors.lua
+++ hypr/desktop/.config/hypr/monitors.lua
@@ -1,11 +1,6 @@
 local display_port = "DP-1"
 
-hl.monitor({
-	output = display_port,
-	mode = "2560x1440@165",
-	position = "0x0",
-	scale = 1,
-})
+hl.monitor({ output = display_port, mode = "2560x1440@165", position = "0x0", scale = 1 })
 
 MONITORS = {
 	MAIN = display_port,
--- hypr/laptop/.config/hypr/monitors.lua
+++ hypr/laptop/.config/hypr/monitors.lua
@@ -3,10 +3,10 @@ local hdmi = "HDMI-A-1"
 local display_port = "DP-1" -- via USB
 
 hl.monitor({ output = built_in, mode = "1920x1080", position = "0x0", scale = 1 })
-hl.monitor({ output = hdmi, mode = "3840x2160", position = "-2560x0", scale = 1.5 })
-hl.monitor({ output = display_port, mode = "3840x2160", position = "0x-1440", scale = 1.5 })
+hl.monitor({ output = hdmi, mode = "3840x2160", position = "auto-left", scale = "auto" })
+hl.monitor({ output = display_port, mode = "3840x2160", position = "auto-up", scale = "auto" })
 
 MONITORS = {
-	MAIN = display_port,
+	MAIN = hdmi,
 	SECONDARY = built_in,
 }
